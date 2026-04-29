a5198a4375e53719713cc8226ebba4a2735e28ac
RodrigoAlmonacid <rodrigo.almonacid@est.fi.uncoma.edu.ar>
2026-04-29 10:21:00 -0300

    optimizo código - se crea PagePrincipal que es el componente utilizado en Favortites y en Home

---
+++ src/components/pagePrincipal/PagePrincipal.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { useLanguage } from "../../context/LanguageContext";
+import { texts } from "../../const/texts";
+import Filter from "../Filters/Filters";
+import Card from "../Card/Card";
+import { Link } from "react-router";
+import { BotonAccion } from "../Button/BotonAction";
+
+function PagePrincipal({filtros, setFiltros, recetario}){
+    const [limite, setLimite] = useState(10);
+    const { lang } = useLanguage();
+    
+return(
+        <div className="flex gap-6 p-6 bg-gray-100 min-h-screen">
+      <aside className="w-1/4">
+        <Filter filtros={filtros} setFiltros={setFiltros} />
+      </aside>
+      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {recetario.length === 0 ? (
+          <p>{texts[lang].noReceta}</p>
+        ) : (
+          recetario.slice(0, limite).map((receta) => (
+            <Link to={`/details/${receta.id}`} key={receta.id}>
+              <Card receta={receta} />
+            </Link>
+          ))
+        )}
+        <div className="flex justify-center mt-6">
+          {limite < recetario.length && (
+            <BotonAccion
+              texto="Cargar más"
+              onClick={() => setLimite(limite + 10)}
+            />
+          )}
+        </div>
+      </section>
+    </div>
+)
+}
+
+export default PagePrincipal;
--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,14 +1,10 @@
-import React, { useState } from "react";
-import Card from "../../components/Card/Card";
+import { useState } from "react";
 //import { recetario } from "../../../recetas_reales";
-import { texts } from "../../const/texts"
 import { useLanguage } from "../../context/LanguageContext";
 import { filtrosRecetas } from "../../utils/filtrado";
-import Filter from "../../components/Filters/Filters";
-import { Link } from "react-router";
-import { BotonAccion } from "../../components/Button/BotonAction";
 import { useEffect } from "react";
 import { getRecetas } from "../../service/api";
+import PagePrincipal from "../../components/pagePrincipal/pagePrincipal";
 
 function Favorites(){
 
@@ -33,33 +29,9 @@ function Favorites(){
   });
     const favoritos = recetario.filter((receta)=>receta.isFavorite===true);
     const favoritosFiltrado = filtrosRecetas(favoritos, filtros, lang);
-    const [limite, setLimite] = useState(10);
 
     return(
-    <div className="flex gap-6 p-6 bg-gray-100 min-h-screen">
-      <aside className="w-1/4">
-        <Filter filtros={filtros} setFiltros={setFiltros} />
-      </aside>
-      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {favoritosFiltrado.length === 0 ? (
-          <p>{texts[lang].noReceta}</p>
-        ) : (
-          favoritosFiltrado.slice(0, limite).map((receta) => (
-            <Link to={`/details/${receta.id}`} key={receta.id}>
-              <Card receta={receta} />
-            </Link>
-          ))
-        )}
-        <div className="flex justify-center mt-6">
-          {limite < favoritosFiltrado.length && (
-            <BotonAccion
-              texto="Cargar más"
-              onClick={() => setLimite(limite + 10)}
-            />
-          )}
-        </div>
-      </section>
-    </div>
+    <PagePrincipal filtros={filtros} setFiltros={setFiltros} recetario={favoritosFiltrado} />
     )
 }
 export default Favorites;
