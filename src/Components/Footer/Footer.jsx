02e2c304f32aa902f2d35eb29123e652c24fff62
HNS5639 <herman.sandoval@est.fi.uncoma.edu.ar>
2026-04-30 00:45:22 -0300

    Delete src directory
    Limpieza para evitar conflictos.

--- src/Components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import "./Styles.css";
-export const Footer = () => {
-  return (
-    <footer className="footer-container">
-      <p>© 2026 TP React - PWA 2026. Todos los derechos reservados.</p>
-      <p>Grupo "+549"</p>
-    </footer>
-  );
-};
