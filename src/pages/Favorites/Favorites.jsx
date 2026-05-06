f132e61bca39215399d6d9445e4d05c914e3c006
RodrigoAlmonacid <rodrigo.almonacid@est.fi.uncoma.edu.ar>
2026-05-06 13:29:26 -0300

    error de tipeo

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -4,7 +4,7 @@ import { useLanguage } from "../../context/LanguageContext";
 import { filtrosRecetas } from "../../utils/filtrado";
 import { useEffect } from "react";
 import { getRecetas } from "../../service/api";
-import PagePrincipal from "../../components/pagePrincipal/pagePrincipal";
+import PagePrincipal from "../../Components/pagePrincipal/pagePrincipal";
 import { useInfiniteScroll } from "../../utils/useInfiniteScroll";
 
 function Favorites() {
